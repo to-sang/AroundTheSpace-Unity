ce80bd4dcd03c32b5560d1c4077065424e82464c
to-sang <tosang0197@gmail.com>
2022-12-19 22:06:32 +0700

    add ghost to space

---
+++ Assets/Script/Monsters/Space/WaypointFollower.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position,transform.position) < .1f)
+        {
+            currentWaypointIndex++;
+            transform.localScale = Vector2.one;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+                transform.localScale = new Vector2(-1, 1);
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position,Time.deltaTime * speed);
+    }
+}
