2fc913065e5eb20a7609bd4ec6327030ce1c94e9
Ngô Ngọc Sáng <ngocsangair01@gmail.com>
2023-01-03 18:22:06 +0700

    fix dialogbox, add sound, add sound main(not done yet)

--- Assets/Script/StoryDialogue.cs
+++ Assets/Script/StoryDialogue.cs
@@ -1,1 +1,75 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using TMPro;  public class StoryDialogue : MonoBehaviour {     public TextMeshProUGUI textComponent;     public string[] lines;     public float speed;     private int index;     // Start is called before the first frame update     void Start()     {         textComponent.text = string.Empty;         StartDialogue();     }      // Update is called once per frame     void Update()     {         if (Input.GetMouseButtonDown(0))         {             if (textComponent.text == lines[index])             {                 NextLine();             }             else             {                 StopAllCoroutines();                 textComponent.text = lines[index];             }         }     }          void StartDialogue()     {         index = 0;         StartCoroutine(TypeLine());      }     IEnumerator TypeLine()     {         foreach (char c in lines[index].ToCharArray())         {             textComponent.text += c;             yield return new WaitForSeconds(speed);         }     }     void NextLine()     {         if (index < lines.Length - 1)         {             index++;             textComponent.text = string.Empty;             StartCoroutine(TypeLine());         }         else         {             gameObject.SetActive(false);         }     } } 
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StoryDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float speed;
+    private int index;
+    private AudioSource audio;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+
+
+    }
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            Sound("storysoud2");
+            textComponent.text += c;
+            yield return new WaitForSeconds(speed);
+        }
+    }
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void Sound(string File)
+    {
+        audio.PlayOneShot(Resources.Load<AudioClip>("Sound/" + File));
+    }
+}
